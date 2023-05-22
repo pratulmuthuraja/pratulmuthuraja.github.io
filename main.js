e0e21118837cdda4affbadd60e18005da826aab4
Pratul Muthuraja <pratulmuthuraja@gmail.com>
2023-05-22 17:48:25 -0500

    Update Summary. Try list group to display list of repos.

--- main.js
+++ main.js
@@ -9,9 +9,7 @@ request.onload = function () {
 
   $.each(data, function (i, status) {
     statusHTML +=
-      "<li><a href='" + status.html_url + "'>" + status.name + "</a></li>";
-  });
-
+      '<div class="list-group"> <a href="' + status.html_url + '" class="list-group-item list-group-item-action" aria-current="true"> <div class="d-flex w-100 justify-content-between"><h5 class="mb-1">' + status.name +'</h5> </div>  <p class="mb-1"></p></a>'; });
   $("#projects-list").html(statusHTML);
 };
 
